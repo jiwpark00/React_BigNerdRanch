d3c4f0087ed06bbd5fa765672cb02b814253e462
jiwpark00 <jiwpark00@gmail.com>
2023-09-22 23:38:08 -0400

    Finishing Chapter 3

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,14 @@
 function Post( { image, name }) {
+    const handleClick = (otterName) => {
+        return console.log('You clicked ' + otterName)
+    }
     return (
         <li className='post-component'>
-            <button>
+            <button onClick={() => handleClick(name)}>
                 <img src={image} alt={name} />
                 <p className="post-name">{name}</p>
             </button>
+
         </li>
     )
 }
